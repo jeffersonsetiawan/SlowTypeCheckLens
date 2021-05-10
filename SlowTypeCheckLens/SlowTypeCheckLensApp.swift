21026e2996b7a52201640e76f65bbc88b5834d06
Jefferson Setiawan <jefferson.setiawan@tokopedia.com>
2021-05-10 18:51:27 +0700

    Initial Commit

---
+++ SlowTypeCheckLens/SlowTypeCheckLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlowTypeCheckLensApp.swift
+//  SlowTypeCheckLens
+//
+//  Created by Jefferson Setiawan on 10/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SlowTypeCheckLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
